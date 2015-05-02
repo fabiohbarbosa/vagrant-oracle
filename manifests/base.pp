e3cc8275419d0c9cbcc34d8020aee11624576a75
Fabio H. G. Barbosa <fabiohbarbosa@gmail.com>
2015-05-02 09:19:12 -0300

    Remove flyaway traces

--- manifests/base.pp
+++ manifests/base.pp
@@ -9,17 +9,3 @@ node oracle {
     require => Service["oracle-xe"],
   }
 }
-
-$a = file('/home/vagrant/vagrant-ubuntu-oracle-xe/oracle-jdbc/ojdbc6.jar', '/dev/null')
-if ($a != '') {
-    # java/maven needed for flyway command
-    class { 'java':
-      distribution => 'jdk',
-    }
-    class { "maven::maven":
-    	version => "3.2.2",
-    }
-    package { 'maven':
-    	ensure => present,
-    }
-}
